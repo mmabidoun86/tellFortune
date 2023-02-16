efa1bd89fd855431c421afca76b86754a45c600d
Mustapha M M <>
2023-02-16 14:43:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+function tellFortune(x,y,z,n){
+  
+  let result = x + y + z + n
+
+  return result
+  
+}
+
+console.log('1. I have a friend, his name is Musty and work as a Software Engineer,', 'He lives in Lagos State, Nigeria.', 'He is happily married and his family name is Mr & Mrs. Akanbi,', 'He has Children; Tow Boys and One Girl. Thank you.,')
+
+
+function tellFortune(x,y,z,n){
+  
+  let result = x + y + z + n
+
+  return result
+  
+}
+
+console.log('2. My uncles name is Mr. Owonikoko and work as a Nepa Officer,', 'He lives in Olowolayemo Town in Osun State.', 'He is married to Mrs. Owodunni and his family name is Mr & Mrs. Owonikoko,', 'He has a Child called; Owolabi. Thank you.,')
+
+
+function tellFortune(x,y,z,n){
+  
+  let result = x + y + z + n
+
+  return result
+  
+}
+
+console.log('3. My name is Mr. Buhari. I am the president of Nigeria,', 'I lives in Aso Rock, Abuja.', 'I mary to Mrs. Aisha Buhari and my family name is Mr & Mrs. Buhari,', 'I have some numbers of Children. Thank you fellowNigerian.,')
+
+
+function calculatorDogAge(Age1,Age2){
+  
+let result = Age1 / Age2
+  
+  return result
+  
+}
+
+console.log(calculatorDogAge(12,7)  + " is the Dog Year")
+
+
+function calculatorDogAge(Age1,Age2){
+  
+let result = Age1 / Age2
+  
+  return result
+  
+}
+
+console.log(calculatorDogAge(12,7)  + " The stated figure is the dog year")
+
+function calculatorDogAge(Age1,Age2){
+  
+let result = Age1 / Age2
+  
+  return result
+  
+}
+
+console.log(calculatorDogAge(12,7)  + " The age of the dog was stated above")
+
+
+function calculateSupply(Age1,Num1){
+  let result = Age1 + Num1
+  return result
+}
+
+console.log("7. helo, the life span of my calculator is "+"3.12 years old")
+
+
+function calculateSupply(Age1,Num1){
+  let result = Age1 + Num1
+  return result
+}
+
+console.log("8. My calculator life span is " + "13 years")
+
+function calcCircumfrence(radius,circumference, NN){
+  let result = radius + circumference + NN
+  return result
+}
+
+console.log("9. 3.14156,", "radius diametre,", "NN")
+
+
+function calcArea(radius,NN){
+  let result = radius + NN
+  return result
+}
+  console.log("10. 3.14156","NN")
+
+function celsiusToFahrenheit(NNC,NNF){
+  let result = NNC - NNF
+  return result
+}
+
+console.log("11. 32°F − 32 × 5/9 = 0°C")
